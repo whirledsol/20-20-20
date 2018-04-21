42e105cbee8f9f1b6caee7a2319458aa6731b1d8
Elliot Potts <elliot@skyzun.net>
2018-04-22 00:39:38 +0100

    Finalised main loop, worked and finished base of inner loop
    Finished the outer loop as best I can, the inner loop, or the one that waits 20 seconds needs work however.

--- 202020.py
+++ 202020.py
@@ -7,13 +7,28 @@ import win32api
 import pyttsx3
 import playsound
 
-tool_start_time = time.time()
+speech_engine = pyttsx3.init()
+speech_engine.setProperty("rate", 200)
+mainLoop_start_time = time.time()
 
 print(" [+] The  tool has been started. Don't disturb this window now!")
 
 while True:
-    loop_time = time.time()
-    if round(loop_time, 1) - round(tool_start_time, 1) == 5:
+    mainLoop_while_time = time.time()
+    if round(mainLoop_while_time, 1) - round(mainLoop_start_time, 1) == 5:
         print(" [!] 20 minutes has passed!")
         playsound.playsound("alert.mp3")
+        speech_engine.say("20 minutes has passed. Look at something 20 feet away, and do this for 20 seconds.")
+        speech_engine.runAndWait()
 
+        innerLoopTime = time.time()
+
+        while True:
+            innerLoopTime_2 = time.time()
+            if round(innerLoopTime_2, 1) - round(innerLoopTime) == 8:
+                print(" [+] 20 seconds has passed!")
+                break
+            else:
+                print("Waiting 8 seconds...")
+
+        mainLoop_start_time = time.time()
